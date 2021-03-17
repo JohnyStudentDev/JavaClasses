17e395a66ff2cd2452ab5b0521678275f3d17638
JohnyStudentDev <76209487+JohnyStudentDev@users.noreply.github.com>
2021-03-17 17:05:06 -0300

    Add files via upload

---
+++ FSort.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class FSort {
+    /**
+     * Method to print an array on the screen
+     * @param array (array)
+     */
+    public void showArray (int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print("["+array[i]+"]");
+        }
+        System.out.println("");
+    }
+
+    /**
+     * Method for applying insertion ordering in an array
+     * @param array (array)
+     */
+    public void insertion (int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
+            int temp = array[i];
+            int j = i - 1;
+            while ((j >= 0) && (array[j] > temp)) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = temp;
+        }
+    }
+
+    /**
+     * Method for applying insertion ordering in an array while printing all steps
+     * @param array (array)
+     */
+    public void showInsertion (int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
+            System.out.print("Insertion["+i+" step]: ");
+            showArray(array);
+            int temp = array[i];
+            int j = i - 1;
+            while ((j >= 0) && (array[j] > temp)) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = temp;
+        }
+        System.out.print("Insertion[Final]: ");
+        showArray(array);
+    }
+}
